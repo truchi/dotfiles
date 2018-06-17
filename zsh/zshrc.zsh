14afd74431ae5d0059ef1d00081b8d34cb476c50
Romain TRUCHI <romain.truchi.06@gmail.com>
2018-06-17 14:18:08 +0200

    ZSH: fixes indentation

--- zsh/zshrc.zsh
+++ zsh/zshrc.zsh
@@ -302,18 +302,18 @@ dog() {
 
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ #
 # ##################################### #
-# > Misc                               #
+# > Misc                                #
 # ##################################### #
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ #
 
 # Man pages colors
-export less_termcap_mb=$'\e[1;31m'     # begin bold
-export less_termcap_md=$'\e[1;36m'     # begin blink
-export less_termcap_me=$'\e[0m'        # reset bold/blink
-export less_termcap_so=$'\e[01;44;33m' # begin reverse video
-export less_termcap_se=$'\e[0m'        # reset reverse video
-export less_termcap_us=$'\e[1;32m'     # begin underline
-export less_termcap_ue=$'\e[0m'        # reset underline
+export less_termcap_mb=$'\e[1;31m'      # begin bold
+export less_termcap_md=$'\e[1;36m'      # begin blink
+export less_termcap_me=$'\e[0m'         # reset bold/blink
+export less_termcap_so=$'\e[01;44;33m'  # begin reverse video
+export less_termcap_se=$'\e[0m'         # reset reverse video
+export less_termcap_us=$'\e[1;32m'      # begin underline
+export less_termcap_ue=$'\e[0m'         # reset underline
 
 # Shut up!
 unsetopt BEEP                           # Beeps on ZLE errors

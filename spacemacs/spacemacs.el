7e049232157cff2c2000ce9cbecaa1034f0a73f4
Romain TRUCHI <romain.truchi.06@gmail.com>
2018-07-30 20:08:52 +0200

    Spacemacs: git hunk status in gutter

--- spacemacs/spacemacs.el
+++ spacemacs/spacemacs.el
@@ -53,7 +53,8 @@ values."
      ;;        shell-default-position 'bottom)
      ;; spell-checking
      ;; syntax-checking
-     ;; version-control
+     (version-control :variables
+                       version-control-diff-side 'left)
      )
    ;; List of additional packages that will be installed without being
    ;; wrapped in a layer. If you need some configuration for these

288fa3b191b87fc81b298d4c5403e68fc8dc0c23
Romain TRUCHI <romain.truchi.06@gmail.com>
2019-04-08 21:09:02 +0200

    (notes)

--- postinstall.zsh
+++ postinstall.zsh
@@ -3,3 +3,9 @@ git submodule update --init --recursive
 
 # Rebuild font cache
 fc-cache -f ~/.fonts
+
+# TODO install spacemacs
+# TODO what if already installed?
+# Maybe prompt 'First install?' then install all the things
+# (npm, node, spacemacs, tern, wakatime, ...)
+# and enable services (emacs, ...?)

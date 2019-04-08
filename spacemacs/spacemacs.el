7916865978b779addd9acb1a1c930477c8ac05e2
Romain TRUCHI <romain.truchi.06@gmail.com>
2019-04-08 23:57:35 +0200

    Ligature hack is unpleasant...

--- spacemacs/spacemacs.el
+++ spacemacs/spacemacs.el
@@ -394,70 +394,70 @@ you should place your code here."
   (add-hook 'evil-visual-state-exit-hook     'my-toggle-whitespace-on)
   ;; NO WHITESPACE IN VISUAL & COMPANY -- END
 
-  ;; FIRA CODE MODE -- START
-  (defun fira-code-mode--make-alist (list)
-    "Generate prettify-symbols alist from LIST."
-    (let ((idx -1))
-      (mapcar
-       (lambda (s)
-         (setq idx (1+ idx))
-         (let* ((code (+ #Xe100 idx))
-                (width (string-width s))
-                (prefix ())
-                (suffix '(?\s (Br . Br)))
-                (n 1))
-           (while (< n width)
-             (setq prefix (append prefix '(?\s (Br . Bl))))
-             (setq n (1+ n)))
-           (cons s (append prefix suffix (list (decode-char 'ucs code))))))
-       list)))
+  ;; ;; FIRA CODE MODE -- START
+  ;; (defun fira-code-mode--make-alist (list)
+  ;;   "Generate prettify-symbols alist from LIST."
+  ;;   (let ((idx -1))
+  ;;     (mapcar
+  ;;      (lambda (s)
+  ;;        (setq idx (1+ idx))
+  ;;        (let* ((code (+ #Xe100 idx))
+  ;;               (width (string-width s))
+  ;;               (prefix ())
+  ;;               (suffix '(?\s (Br . Br)))
+  ;;               (n 1))
+  ;;          (while (< n width)
+  ;;            (setq prefix (append prefix '(?\s (Br . Bl))))
+  ;;            (setq n (1+ n)))
+  ;;          (cons s (append prefix suffix (list (decode-char 'ucs code))))))
+  ;;      list)))
 
-  (defconst fira-code-mode--ligatures
-    '("www" "**" "***" "**/" "*>" "*/" "\\\\" "\\\\\\"
-      "{-" "[]" "::" ":::" ":=" "!!" "!=" "!==" "-}"
-      "--" "---" "-->" "->" "->>" "-<" "-<<" "-~"
-      "#{" "#[" "##" "###" "####" "#(" "#?" "#_" "#_("
-      ".-" ".=" ".." "..<" "..." "?=" "??" ";;" "/*"
-      "/**" "/=" "/==" "/>" "//" "///" "&&" "||" "||="
-      "|=" "|>" "^=" "$>" "++" "+++" "+>" "=:=" "=="
-      "===" "==>" "=>" "=>>" "<=" "=<<" "=/=" ">-" ">="
-      ">=>" ">>" ">>-" ">>=" ">>>" "<*" "<*>" "<|" "<|>"
-      "<$" "<$>" "<!--" "<-" "<--" "<->" "<+" "<+>" "<="
-      "<==" "<=>" "<=<" "<>" "<<" "<<-" "<<=" "<<<" "<~"
-      "<~~" "</" "</>" "~@" "~-" "~=" "~>" "~~" "~~>" "%%"
-      "x" ":" "+" "+" "*"))
+  ;; (defconst fira-code-mode--ligatures
+  ;;   '("www" "**" "***" "**/" "*>" "*/" "\\\\" "\\\\\\"
+  ;;     "{-" "[]" "::" ":::" ":=" "!!" "!=" "!==" "-}"
+  ;;     "--" "---" "-->" "->" "->>" "-<" "-<<" "-~"
+  ;;     "#{" "#[" "##" "###" "####" "#(" "#?" "#_" "#_("
+  ;;     ".-" ".=" ".." "..<" "..." "?=" "??" ";;" "/*"
+  ;;     "/**" "/=" "/==" "/>" "//" "///" "&&" "||" "||="
+  ;;     "|=" "|>" "^=" "$>" "++" "+++" "+>" "=:=" "=="
+  ;;     "===" "==>" "=>" "=>>" "<=" "=<<" "=/=" ">-" ">="
+  ;;     ">=>" ">>" ">>-" ">>=" ">>>" "<*" "<*>" "<|" "<|>"
+  ;;     "<$" "<$>" "<!--" "<-" "<--" "<->" "<+" "<+>" "<="
+  ;;     "<==" "<=>" "<=<" "<>" "<<" "<<-" "<<=" "<<<" "<~"
+  ;;     "<~~" "</" "</>" "~@" "~-" "~=" "~>" "~~" "~~>" "%%"
+  ;;     "x" ":" "+" "+" "*"))
 
-  (defvar fira-code-mode--old-prettify-alist)
+  ;; (defvar fira-code-mode--old-prettify-alist)
 
-  (defun fira-code-mode--enable ()
-    "Enable Fira Code ligatures in current buffer."
-    (setq-local fira-code-mode--old-prettify-alist prettify-symbols-alist)
-    (setq-local prettify-symbols-alist
-                (append
-                 (fira-code-mode--make-alist fira-code-mode--ligatures)
-                 fira-code-mode--old-prettify-alist))
-    (prettify-symbols-mode t))
+  ;; (defun fira-code-mode--enable ()
+  ;;   "Enable Fira Code ligatures in current buffer."
+  ;;   (setq-local fira-code-mode--old-prettify-alist prettify-symbols-alist)
+  ;;   (setq-local prettify-symbols-alist
+  ;;               (append
+  ;;                (fira-code-mode--make-alist fira-code-mode--ligatures)
+  ;;                fira-code-mode--old-prettify-alist))
+  ;;   (prettify-symbols-mode t))
 
-  (defun fira-code-mode--disable ()
-    "Disable Fira Code ligatures in current buffer."
-    (setq-local prettify-symbols-alist fira-code-mode--old-prettify-alist)
-    (prettify-symbols-mode -1))
+  ;; (defun fira-code-mode--disable ()
+  ;;   "Disable Fira Code ligatures in current buffer."
+  ;;   (setq-local prettify-symbols-alist fira-code-mode--old-prettify-alist)
+  ;;   (prettify-symbols-mode -1))
 
-  (define-minor-mode fira-code-mode
-    "Fira Code ligatures minor mode"
-    :lighter " Fira Code"
-    (setq-local prettify-symbols-unprettify-at-point 'right-edge)
-    (if fira-code-mode
-        (fira-code-mode--enable)
-      (fira-code-mode--disable)))
+  ;; (define-minor-mode fira-code-mode
+  ;;   "Fira Code ligatures minor mode"
+  ;;   :lighter " Fira Code"
+  ;;   (setq-local prettify-symbols-unprettify-at-point 'right-edge)
+  ;;   (if fira-code-mode
+  ;;       (fira-code-mode--enable)
+  ;;     (fira-code-mode--disable)))
 
-  (defun fira-code-mode--setup ()
-    "Setup Fira Code Symbols"
-    (set-fontset-font t '(#Xe100 . #Xe16f) "Fira Code Symbol"))
-  (provide 'fira-code-mode)
+  ;; (defun fira-code-mode--setup ()
+  ;;   "Setup Fira Code Symbols"
+  ;;   (set-fontset-font t '(#Xe100 . #Xe16f) "Fira Code Symbol"))
+  ;; (provide 'fira-code-mode)
 
-  (add-hook 'prog-mode-hook 'fira-code-mode)
-  ;; FIRA CODE MODE -- END
+  ;; (add-hook 'prog-mode-hook 'fira-code-mode)
+  ;; ;; FIRA CODE MODE -- END
 
   ;; LOCAL ESLINT BIN FOR FLYCHECK -- START
   (defun my/use-eslint-from-node-modules ()
